dba4001b8496197f0de4ed5f59e9922693220318
JeetChauhan17 <121753279+JeetChauhan17@users.noreply.github.com>
2023-12-24 18:54:24 +0530

    Update app.js

--- project_draft_hero/app.js
+++ project_draft_hero/app.js
@@ -1,41 +1,47 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
-    const imageUrls = [
-        "media/1.jpg",
-        "media/2.jpg",
-        "media/3.jpg",
-        "media/4.jpg",
-        "media/5.jpg",
-        "media/6.jpg",
-        "media/7.jpg",
-        "media/8.jpg",
-        "media/9.jpg",
-        "media/10.jpg",
-        "media/11.jpg",
-        "media/12.jpg",
-        "media/13.jpg",
-        "media/14.jpg",
-      ];
-    
-  
-    const items = document.querySelectorAll(".item");
-  
-    items.forEach((item, index) => {
-      const imageUrl = imageUrls[index];
-      item.style.backgroundImage = `url(${imageUrl})`;
-    });
-  
-    let next = document.querySelector(".next");
-    let prev = document.querySelector(".prev");
-  
-    next.addEventListener("click", function () {
-      let items = document.querySelectorAll(".item");
-      document.querySelector(".slide").appendChild(items[0]);
-    });
-  
-    prev.addEventListener("click", function () {
-      let items = document.querySelectorAll(".item");
-      document.querySelector(".slide").prepend(items[items.length - 1]);
-    });
+  const imageUrls = [
+    "https://i.ibb.co/R78M75s/1.jpg",
+    "https://i.ibb.co/RH50FJ0/2.jpg",
+    "https://i.ibb.co/qgV3st2/3.jpg",
+    "https://i.ibb.co/Dr75bc3/4.jpg",
+    "https://i.ibb.co/sm81dvK/5.jpg",
+    "https://i.ibb.co/wR7pgGB/6.jpg",
+    "https://i.ibb.co/yY37Mqz/7.jpg",
+    "https://i.ibb.co/1sZ84hP/8.jpg",
+    "https://i.ibb.co/n1p9W34/9.jpg",
+    "https://i.ibb.co/BsDr9q0/10.jpg",
+    "https://i.ibb.co/tqdNKXB/11.jpg",
+    "https://i.ibb.co/5cYgDMV/12.jpg",
+    "https://i.ibb.co/7b3j16R/13.jpg",
+    "https://i.ibb.co/YW5gCph/14.jpg",
+  ];
+
+  const items = document.querySelectorAll(".item");
+
+  // items.forEach((item, index) => {
+  //   const imageUrl = imageUrls[index];
+  //   const img = document.createElement("img");
+  //   img.src = imageUrl;
+  //   item.appendChild(img);
+  // });
+
+  // const items = document.querySelectorAll(".item");
+
+  items.forEach((item, index) => {
+    const imageUrl = imageUrls[index];
+    item.style.backgroundImage = `url(${imageUrl})`;
   });
+
+  let next = document.querySelector(".next");
+  let prev = document.querySelector(".prev");
+
+  next.addEventListener("click", function () {
+    let items = document.querySelectorAll(".item");
+    document.querySelector(".slide").appendChild(items[0]);
+  });
+
+  prev.addEventListener("click", function () {
+    let items = document.querySelectorAll(".item");
+    document.querySelector(".slide").prepend(items[items.length - 1]);
+  });
+});
